6edb3316fd788e0389f2bf2bc25c0052f5e36bdd
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-23 18:19:56 +0100

    add State

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Accordion = ({ items }) => {
+
+    const [activeIndex,setActiveIndex]=useState(null);
+
   const renderedItems = items.map((item, index) => {
-   const handleClick=(index)=>{
-console.log('you clicked me', index)
-   }
+    const onTitleClick = (index) => {
+      setActiveIndex(index);
+    };
 
     return (
-      <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => handleClick(index)}>
+      <React.Fragment key={index}>
+        <div className="title active" onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
         <div className="content active">
           <p>{item.content}</p>
         </div>
-        
       </React.Fragment>
     );
   });
 
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return <div className="ui styled accordion">{renderedItems}
+  <h1>{activeIndex}</h1>
+  </div>;
 };
 export default Accordion;
