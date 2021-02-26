1bf1af56f68765d477bd07de814d64bf2f546ef1
Mani Movassagh <mani.mgh@gmail.com>
2021-02-26 16:39:54 +0100

    search3

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 
 const Search = () => {
+
   const [term, setTerm] = useState("");
+
+
   return (
     <div className="ui form">
       <div className="field">
         <label>Enter your Search</label>
-        <input onChange="" className="input" />
+        <input value={term} onChange={e => setTerm(e.value)} className="input" />
       </div>
     </div>
   );
