ebaa1b05bca7f47fa9bfbd66668311f2e30d0781
Mani Movassagh <mani.mgh@gmail.com>
2021-02-26 15:42:06 +0100

    search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
 // const wikiApi="https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=Craig%20Noone"
 const Search = () => {
-    return ( 
-        <h1> search </h1>
-     );
-}
- 
+  return (
+    <div className="ui form">
+      <div className="field">
+        <label>Enter your Search</label>
+        <input type="text" className="input" />
+      </div>
+    </div>
+  );
+};
+
 export default Search;
